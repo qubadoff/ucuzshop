59f17c4b4cbec9d3b20d7a5b9d3a47eb2d6519f1
qubadoff <admin@burncode.org>
2024-08-30 01:40:42 +0400

    i

---
+++ app/Filament/Resources/EmployeeResource.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\EmployeeResource\Pages;
+use App\Models\Employee;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class EmployeeResource extends Resource
+{
+    protected static ?string $model = Employee::class;
+
+    protected static ?string $navigationLabel = 'İşçilər';
+
+    protected static ?string $label = 'İşçi';
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make([
+                    TextInput::make('name')->required()->label('Ad'),
+                    TextInput::make('phone')->required()->label('Telefon'),
+                    TextInput::make('email')->email()->label('Email'),
+                    TextInput::make('password')->password()->label('Şifrə'),
+                ])->columns('4'),
+                Section::make([
+                    Textarea::make('description')->label('Təsvir'),
+                    FileUpload::make('image')->image()->label('Şəkil'),
+                    Select::make('status')->label('Status')->options([
+                        '1' => 'Aktiv',
+                        '2' => 'Deaktiv',
+                    ])->default('1')
+                ])->columns('3')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')->sortable(),
+                Tables\Columns\TextColumn::make('name')->label('Ad')->searchable(),
+                Tables\Columns\TextColumn::make('phone')->label('Telefon'),
+                Tables\Columns\TextColumn::make('created_at')->label('Əlavə olundu')->date(),
+                Tables\Columns\TextColumn::make('updated_at')->label('Yeniləndi')->date(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListEmployees::route('/'),
+            'create' => Pages\CreateEmployee::route('/create'),
+            'edit' => Pages\EditEmployee::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ database/migrations/2024_08_30_005905_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->string('phone');
+            $table->text('description')->nullable();
+            $table->text('image')->nullable();
+            $table->text('password');
+            $table->integer('status')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
