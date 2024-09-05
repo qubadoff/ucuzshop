8fb7cf912b5aa5b5fd2b5e5a3699e43c20565c00
qubadoff <admin@burncode.org>
2024-09-05 12:28:45 +0400

    Improvements

--- resources/views/Frontend/layouts/includes/header.blade.php
+++ resources/views/Frontend/layouts/includes/header.blade.php
@@ -59,12 +59,6 @@
                                     <i class="flaticon-shopping-cart"></i>
                                 </a>
                             </li>
-                            <li class="shopping-cart">
-                                <a href="#" class="cart-dropdown-btn">
-                                    <span class="cart-count">3</span>
-                                    <i class="flaticon-shopping-cart"></i>
-                                </a>
-                            </li>
                         @endif
                         <li class="my-account">
                             <a href="javascript:void(0)">
