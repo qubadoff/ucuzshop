3444c7f222458ba4402141667121ac6437e4f4da
qubadoff <admin@burncode.org>
2024-09-05 06:42:16 +0400

    Improvements

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,6 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Blue,
             ])
+            ->profile()
             ->maxContentWidth(MaxWidth::Full)
             ->sidebarWidth(100)
             ->sidebarFullyCollapsibleOnDesktop()
