aec80e92f7c7c8bb23d4d1ff91c4c419f60a8f5a
qubadoff <admin@burncode.org>
2024-09-25 03:50:00 +0400

    Improvements

--- resources/views/Frontend/cart/index.blade.php
+++ resources/views/Frontend/cart/index.blade.php
@@ -15,11 +15,10 @@
                             <thead>
                             <tr>
                                 <th scope="col" class="product-remove"></th>
-                                <th scope="col" class="product-thumbnail">Product</th>
-                                <th scope="col" class="product-title"></th>
-                                <th scope="col" class="product-price">Price</th>
-                                <th scope="col" class="product-quantity">Quantity</th>
-                                <th scope="col" class="product-subtotal">Subtotal</th>
+                                <th scope="col" class="product-thumbnail">Məhsul</th>
+                                <th scope="col" class="product-price">Qiymət</th>
+                                <th scope="col" class="product-quantity">Say</th>
+                                <th scope="col" class="product-subtotal">Ümumi məbləğ</th>
                             </tr>
                             </thead>
                             <tbody>
